d29d3fcd43232ae2b22e4ca997585a8b5145c554
Carlos Olivares <calchuchesta@carloss-mbp.home>
2021-01-04 20:59:17 -0500

    A quick formula to calclate BMI. DOM and Events are up next.

---
+++ js/Section-2-codingChallenge-solutions.js
@@ -0,0 +1,23 @@
+// Javascript fundamentals - Part 1 coding challenge solutions
+
+/*
+// Coding challenge 1
+
+const markHeight = 1.69;
+const markMass = 78;
+
+const johnHeight = 1.95;
+const johnMass = 92;
+
+function calculateBmi(height, mass) {
+    return ( mass / ( height * height ) );
+    }
+
+const markHigherBMI = ( calculateBmi(markHeight, markMass) > calculateBmi(johnHeight, johnMass));
+
+console.log(markHigherBMI);
+*/
+
+// Coding Challenge 2
+
+
