b9a54481acb5c679a7166346a6ce0fb157c851a3
Carlos Olivares <calchuchesta@carloss-mbp.home>
2021-01-09 14:02:03 -0500

    Started Challenge 3 of section 2. I am struggling a lot more than I originally thought. When I come back, I will make some function methods instead and try to use the this keyword so the code makes more sense.
    I would prefer to define a class instead of having to make so many
    object literals, but I have not been taught this yet.

--- js/Section-2-codingChallenge-solutions.js
+++ js/Section-2-codingChallenge-solutions.js
@@ -20,4 +20,42 @@ console.log(markHigherBMI);
 
 // Coding Challenge 2
 
+// too boring. skip to 3.
 
+
+//                             coding challenge 3
+
+// data first:
+
+const data1 = {
+    dolphinsScores:  [96, 108, 89],
+    koalasScores: [88, 91, 110]
+}
+
+const data2 = {
+    dolphinsScores:  [97, 112, 101],
+    koalasScores: [109, 95, 123]
+}
+
+const data3 = {
+    dolphinsScores:  [97, 112, 101],
+    koalasScores: [109, 95, 106]
+}
+
+function getAvg(lst){
+    let sum = 0;
+    for (const val of lst){
+        sum += val;
+    }
+    return ( sum / (lst.length));
+}
+
+function getAvgScores(data){
+    data['dolphinsAvg'] = getAvg(data.dolphinsScores);
+    data['koalasAvg'] = getAvg(data.koalasScores);
+
+}
+
+function checkMins(data){
+    if ((data.dolphinsAvg < 100) && (data.koalasAvg < 100)) console.log('No one wins!');
+}
