ff5ae9e792c1d16996ea0473d8fa301157ef28bf
Carlos Olivares <calchuchesta@carloss-mbp.home>
2021-01-10 16:53:47 -0500

    Code examples for Section 10: A Closer Look at Functions: First-Class and Higher-Order Functions.
    No code examples were really needed for this lecture. Everything covered
    will also have its own lecture, so we will see.

--- js/following_lectures.js
+++ js/following_lectures.js
@@ -21,3 +21,28 @@ billStatement(400);
 
 // if you want to skip a parameter with a default value, put undefined.
 // As a best practice, put the parameters with default options last...
+
+/* Section 10: A Closer Look at Functions: How Passing Parameters Works: Value vs. Reference  */
+
+const passenger = {
+    name: 'Drew Barrymore',
+    age: 200780
+};
+
+const flight = 'LH234';
+
+function boardingStatement(passenger, flight) {
+    passenger.name = 'Ms. ' + passenger.name;
+    print(`${name} is boarding flight ${flight}`);
+}
+
+boardingStatement(passenger, flight);
+print(flight); // this value does not change. It was passed by value
+print(passenger.name); //this value does change. It was passed by reference.
+
+/* Section 10: A Closer Look at Functions: First-Class and Higher-Order Functions */
+
+// We can call methods on functions when they are first class functions.
+//
+// higher order functions can accept a fn as a param, OR return a function.
+// higher order function require first class functions
