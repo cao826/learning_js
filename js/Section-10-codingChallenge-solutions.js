5954fb46602ed37eadb1c5725451db1c1c29cb87
Carlos Olivares <carlos.olivares.331@outlook.com>
2021-01-13 17:32:06 -0500

    Added Preliminary solution for Section 10 coding challenge 1
    Needs to be fixed, but so do all/ most of them. I will make an issue
    about this.

--- js/Section-10-codingChallenge-solutions.js
+++ js/Section-10-codingChallenge-solutions.js
@@ -1,10 +1,66 @@
                    /* Section 10 Coding Challenge 1 */
 
 // Starter code for coding challenge 1
+const acceptableAnswers = new Set(['0', '1', '2', '3']);
+
+const checkInput = function (input) {
+    if (acceptableAnswers.has(input)) {
+        console.log('input was acceptable!');
+        return true; 
+    } else {
+        console.log('User input is invalid!');
+        return false;
+    }
+}
+
 const poll = {
 question: "What is your favourite programming language?", 
     options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
     answers: new Array(4).fill(0),
+    getInput() {
+        console.log(this.question);
+        console.log(this.options);
+        console.log(this.options.join('\n'));
+        let promptString = this.question + "\n" + this.options.join('\n');
+        console.log(promptString);
+        const userResponse = prompt(promptString);
+        console.log(userResponse, typeof userResponse);
+        return userResponse;
+    },
+    updatePoll(userInput) {
+        if (checkInput(userInput)) {
+            console.log('I am adding to the answers section!)');
+            const index = Number(userInput);
+            this.answers[index]++;
+            console.log(this.answers);
+            return true;
+        }
+        return false;
+    },
+    showResults(type){
+        switch (type) {
+            case 'array':
+                console.log(this.answers);
+                break;
+            case 'string':
+                const why = [...this.answers]; 
+                for (let i = 0; i < why.length; i++){
+                    why[i] = why[i].toString();
+                }
+                console.log(`Poll results are ${why.join(',')}`);
+                break;
+            default:
+                console.log(this.answers);
+                break;
+        }
+    },
+    runPoll(){
+        const userResponse = this.getInput();
+        let updateStatus = this.updatePoll(userResponse);
+        if (updateStatus){
+            this.showResults('string');
+        }
+    }
 };
 
-
+document.querySelector('.poll').addEventListener('click', poll.runPoll.bind(poll));
